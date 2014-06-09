4778348a03722f7b370d95fb589413d72ebb756f
Alexey Melnichuk <mimir@newmail.ru>
2014-06-09 11:26:30 +0500

    Update Travis files.

--- .travis/setup_lua.sh
+++ .travis/setup_lua.sh
@@ -1,12 +1,33 @@
+#! /bin/bash
+
 # A script for setting up environment for travis-ci testing.
 # Sets up Lua and Luarocks.
 # LUA must be "lua5.1", "lua5.2" or "luajit".
 # PLATFORM must be "linux" or "macosx".
 
+LUAJIT_BASE="LuaJIT-2.0.3"
+
+if [ -z "$PLATFORM" ]; then
+  PLATFORM=$TRAVIS_OS_NAME;
+fi
+
+if [ "$PLATFORM" == "osx" ]; then
+  PLATFORM="macosx";
+fi
+
+if [ -z "$PLATFORM" ]; then
+  if [ "$(uname)" == "Linux" ]; then
+    PLATFORM="linux";
+  else
+    PLATFORM="macosx";
+  fi;
+fi
+
 if [ "$LUA" == "luajit" ]; then
-  curl http://luajit.org/download/LuaJIT-2.0.3.tar.gz | tar xz
-  cd LuaJIT-2.0.3
+  curl http://luajit.org/download/$LUAJIT_BASE.tar.gz | tar xz
+  cd $LUAJIT_BASE
   make && sudo make install
+  sudo ln -s /usr/local/bin/luajit /usr/local/bin/lua
   cd $TRAVIS_BUILD_DIR;
 else
   if [ "$LUA" == "lua5.1" ]; then
@@ -20,7 +41,7 @@ else
   cd $TRAVIS_BUILD_DIR;
 fi
 
-LUAROCKS_BASE=luarocks-$LUAROCKS_VER
+LUAROCKS_BASE=luarocks-$LUAROCKS
 curl http://luarocks.org/releases/$LUAROCKS_BASE.tar.gz | tar xz
 cd $LUAROCKS_BASE;
 
@@ -30,6 +51,16 @@ else
   ./configure;
 fi
 
-make && sudo make install
+make build && sudo make install
 
 cd $TRAVIS_BUILD_DIR
+
+rm -rf $LUAROCKS_BASE
+
+if [ "$LUA" == "luajit" ]; then
+  rm -rf $LUAJIT_BASE;
+elif [ "$LUA" == "lua5.1" ]; then
+  rm -rf lua-5.1.5;
+elif [ "$LUA" == "lua5.2" ]; then
+  rm -rf lua-5.2.3;
+fi
